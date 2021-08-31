f8855f95fc26ad0d2bd4d1dec05d8e1a185aacb9
Omer Erdem Aydemir <aydemirerdemomer@gmail.com>
2021-08-31 18:13:40 +0300

    I writed service for spacex api

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:spacex/view/spacex_view.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(
+    MyApp(),
+  );
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
-    );
+        title: 'SpaceX',
+        home: MultiProvider(providers: [
+          // ChangeNotifierProvider(create: (_) => SpaceXViewModel()),
+        ], child: SpaceXView()));
   }
 }
