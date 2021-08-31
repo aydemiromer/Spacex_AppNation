5a45181e9917244254d857c53af85f4826abbe12
Omer Erdem Aydemir <aydemirerdemomer@gmail.com>
2021-08-31 20:03:09 +0300

    Elon musk said hi

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:spacex/view-model/spacex_view_model.dart';
 import 'package:spacex/view/spacex_view.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 
 void main() {
   //WidgetsFlutterBinding.ensureInitialized();
@@ -14,12 +15,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'SpaceX',
-      home: MultiProvider(providers: [
-        ChangeNotifierProvider<SpaceXListViewModel>(
-            create: (_) => SpaceXListViewModel()),
-      ], child: SpaceXView()),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'SpaceX',
+        home: AnimatedSplashScreen(
+          duration: 1600,
+          splash: Image.network(
+              'https://image.cnbcfm.com/api/v1/image/106836975-1612791996353-gettyimages-1206289333-SATELLITE_2020.jpeg?v=1626178796'),
+          nextScreen: MultiProvider(providers: [
+            ChangeNotifierProvider<SpaceXListViewModel>(
+                create: (_) => SpaceXListViewModel()),
+          ], child: SpaceXView()),
+          splashTransition: SplashTransition.fadeTransition,
+          backgroundColor: Colors.black38,
+        ));
   }
 }
